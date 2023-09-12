5fd98f4cf4849d7234db50e56781825340300a5d
willgc88 <william.clayton@mail.utoronto.ca>
2023-09-12 15:38:28 -0400

    final submission revised

--- week1git/Multiples.java
+++ week1git/Multiples.java
@@ -1,11 +1,21 @@
 public class Multiples {
-    public static void main(String[] args) {
+    public static int main(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int main() {
         int count = 0;
         for (int i = 1; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 count++;
             }
         }
-        System.out.println(count);
+        return count;
     }
 }
--- week1git/Reduce.java
+++ week1git/Reduce.java
@@ -1,15 +1,15 @@
 public class Reduce {
-    public static void main(String[] args) {
-        int n = 100;
+    public static int main(int n) {
+        int x = n;
         int count = 0;
-        while (n != 0){
-            if (n % 2 == 0){
-                n = n / 2;
+        while (x != 0){
+            if (x % 2 == 0){
+                x = x / 2;
             }else{
-                n = n - 1;
+                x = x - 1;
             }
             count++;
         }
-        System.out.println(count);
+        return count;
     }
 }
